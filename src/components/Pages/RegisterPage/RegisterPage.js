01738f04f25e027494cfa6f4574c312c5e91ad22
Jordan Walker <jwalker117@gmail.com>
2020-06-17 16:12:37 -0500

    Added user validation and have the input fields going to sagas and logging in new user

--- src/components/Pages/RegisterPage/RegisterPage.js
+++ src/components/Pages/RegisterPage/RegisterPage.js
@@ -14,28 +14,40 @@ class RegisterPage extends Component {
     last_name: '',
     password: '',
     confirm_pwd: '',
+    validated: false,
   };
 
+  setValidated = () => {
+    this.setState({validated: true});
+  }
+
   registerUser = (event) => {
-    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    this.setValidated();
+
 
     console.log('state is: ',this.state);
     //Things to check before dispatching to the server
     //input validation should be performed here for creating a user
-    // if (this.state.username && this.state.password) {
-    //   this.props.dispatch({
-    //     type: 'REGISTER',
-    //     payload: {
-    //       username: this.state.username,
-    //       email: this.state.email,
-    //       first_name: this.state.first_name,
-    //       last_name: this.state.last_name,
-    //       password: this.state.password,
-    //     },
-    //   });
-    // } else {
-    //   this.props.dispatch({type: 'REGISTRATION_INPUT_ERROR'});
-    // }
+    if (this.state.username && this.state.password === this.state.confirm_pwd) {
+      this.props.dispatch({
+        type: 'REGISTER',
+        payload: {
+          username: this.state.username,
+          email: this.state.email,
+          first_name: this.state.first_name,
+          last_name: this.state.last_name,
+          password: this.state.password,
+        },
+      });
+    } else {
+      this.props.dispatch({type: 'REGISTRATION_INPUT_ERROR'});
+    }
   } // end registerUser
 
   handleInputChangeFor = propertyName => (event) => {
@@ -58,71 +70,111 @@ class RegisterPage extends Component {
         <Card>
           <Card.Body>
           <Card.Title>Create New Account</Card.Title>
-            <Form onSubmit={this.registerUser}>
+          <hr />
+            <Form 
+              onSubmit={this.registerUser}
+              noValidate
+              validated={this.state.validated}
+            >
 
               {/* Username Field */}
               <Form.Group controlId="formBasicUsername">
                 <Form.Label>Username</Form.Label>
                 <Form.Control 
-                  type="text" 
+                  type="text"
+                  pattern="[A-Za-z0-9]{4,}"
+                  required
                   placeholder="Enter Username"
                   value={this.state.username}
                   onChange={this.handleInputChangeFor('username')}
                 />
+                <Form.Control.Feedback>
+                  Username good!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                  Please choose a username that is 6 characters in length.
+                </Form.Control.Feedback>
               </Form.Group>
 
               {/* First Name Field */}
               <Form.Group controlId="formBasicFirstName">
                 <Form.Label>First Name</Form.Label>
                 <Form.Control 
-                  type="text" 
+                  type="text"
+                  pattern="([A-Za-z]){2,30}"
+                  required 
                   placeholder="Enter First Name"
                   value={this.state.first_name}
                   onChange={this.handleInputChangeFor('first_name')}
                 />
+                <Form.Control.Feedback>
+                  Looks good!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                  Please enter First Name.
+                </Form.Control.Feedback>
               </Form.Group>
 
               {/* Last Name Field */}
               <Form.Group controlId="formBasicLastName">
                 <Form.Label>Last Name</Form.Label>
                 <Form.Control 
-                  type="text" 
+                  type="text"
+                  pattern="[A-Za-z]{2,}"
+                  required 
                   placeholder="Enter Last Name"
                   value={this.state.last_name}
                   onChange={this.handleInputChangeFor('last_name')}
                 />
+                <Form.Control.Feedback>
+                  Looks good!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                  Please enter Last Name.
+                </Form.Control.Feedback>
               </Form.Group>
 
               {/* Email Field */}
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control 
-                  type="email" 
+                  type="email"
+                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                  required 
                   placeholder="Enter email"
                   value={this.state.email}
                   onChange={this.handleInputChangeFor('email')}
                 />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
+                <Form.Control.Feedback>
+                  Email is good!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                  Please enter valid email address.
+                </Form.Control.Feedback>
               </Form.Group>
 
               {/* Password Field */}
               <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control 
-                  type="password" 
+                  type="password"
+                  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}"
+                  required 
                   placeholder="Enter Password"
                   value={this.state.password}
                   onChange={this.handleInputChangeFor('password')} 
                 />
+                <Form.Control.Feedback type="invalid">
+                  Must contain at least one number and one uppercase and lowercase letter, and at least 8 to 20 characters
+                </Form.Control.Feedback>
               </Form.Group>
 
               {/* Confirm Password Field */}
               <Form.Group controlId="formBasicConfirmPassword">
                 <Form.Label>Confirm Password</Form.Label>
                 <Form.Control 
-                  type="password" 
+                  type="password"
+                  required 
                   placeholder="Confirm Password"
                   value={this.state.confirm_pwd}
                   onChange={this.handleInputChangeFor('confirm_pwd')} 
